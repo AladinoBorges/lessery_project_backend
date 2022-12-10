d14f7a428d83c8bfef8780a8d065ece8a9d4ae19
Aladino Borges <aladino@huntly.com.br>
2022-12-10 20:43:47 -0300

    feat[utils]: creates logging and cache settings utility python file

---
+++ utils/config.py
@@ -0,0 +1,15 @@
+import logging
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+from classes.SettingsClass import SettingsClass
+
+log = logging.getLogger("uvicorn")
+
+
+@lru_cache
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from environment ...")
+
+    return SettingsClass()
