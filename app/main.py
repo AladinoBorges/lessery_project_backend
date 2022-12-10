59fafa880adccd5e4c6e0ca578be41bf14ade448
Aladino Borges <aladino@huntly.com.br>
2022-12-10 20:44:33 -0300

    feat[app]: creates main python file with:
    - basic implementation of fast api home and ping endpoints

---
+++ app/main.py
@@ -0,0 +1,50 @@
+from fastapi import Depends, FastAPI
+
+from classes.DefaultResponse import DefaultResponse
+from classes.SettingsClass import SettingsClass
+from utils.config import get_settings
+
+app = FastAPI()
+
+
+def settings_data() -> SettingsClass:
+    return Depends(get_settings)
+
+
+@app.get("/")
+async def home(
+    settings: SettingsClass = settings_data(),
+) -> dict[str, int | bool | str | None | dict | list]:
+    response = DefaultResponse(
+        200,
+        "welcome to the homepage.",
+        settings.environment,
+        settings.testing,
+    )
+
+    return response.success
+
+
+@app.get("/ping")
+async def ping(
+    settings: SettingsClass = settings_data(),
+) -> dict[str, int | bool | str | None | dict | list]:
+    response = DefaultResponse(
+        200, "pong.", settings.environment, settings.testing
+    )
+
+    return response.success
+
+
+@app.get("*")
+async def not_found(
+    settings: SettingsClass = settings_data(),
+) -> dict[str, int | bool | str | None | dict | list]:
+    response = DefaultResponse(
+        404,
+        "ups! i did it again, 404.",
+        settings.environment,
+        settings.testing,
+    )
+
+    return response.success
