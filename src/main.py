eb7fa8c6110e813b48991a8ea6c068894a5ba19d
Aladino Borges <aladino@huntly.com.br>
2022-12-11 16:55:29 -0300

    feat[appArchitecture]: updates architecture for orm implementation
    - src main and __init__

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+from src.api import status
+from src.utils.config import shutdown_event, startup_event
+
+
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(status.router, tags=["Hello"], prefix="/api/v1")
+
+    return application
+
+
+app: FastAPI = create_application()
+
+
+@app.on_event("startup")
+async def generate_startup_log() -> None:
+    startup_event()
+
+
+@app.on_event("shutdown")
+async def generate_shutdown_log() -> None:
+    shutdown_event()
