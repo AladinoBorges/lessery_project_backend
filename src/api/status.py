1bc8586cd56c9d8558c85610b488cb1ab4fb3518
Aladino Borges <aladino@huntly.com.br>
2022-12-11 16:56:00 -0300

    feat[appArchitecture]: updates architecture for orm implementation
    - creates api routes: status route

---
+++ src/api/status.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, Depends
+
+from src.classes.DefaultResponse import DefaultResponse
+from src.classes.SettingsClass import SettingsClass
+from src.utils.config import get_settings
+
+router = APIRouter()
+
+
+def settings_data() -> SettingsClass:
+    return Depends(get_settings)
+
+
+@router.get("/status")
+async def ping(
+    settings: SettingsClass = settings_data(),
+) -> dict[str, int | bool | str | None | dict | list]:
+    response = DefaultResponse(
+        200,
+        "pong.",
+        settings.version,
+        settings.environment,
+        settings.testing,
+    )
+
+    return response.success
+
+
+@router.get("*")
+async def not_found(
+    settings: SettingsClass = settings_data(),
+) -> dict[str, int | bool | str | None | dict | list]:
+    response = DefaultResponse(
+        404,
+        "ups! i did it again, 404.",
+        settings.environment,
+        settings.testing,
+    )
+
+    return response.success
