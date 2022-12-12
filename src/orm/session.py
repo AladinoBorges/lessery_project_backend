af03e66d0c3602801befee5088b7f71d1046723a
Aladino Borges <aladino@huntly.com.br>
2022-12-11 22:04:26 -0300

    refactor[orm]: session initializer logic

--- src/orm/session.py
+++ src/orm/session.py
@@ -1,56 +1,32 @@
 import os
-from typing import Callable, Optional
 
-import sqlalchemy
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
 
 from src.orm.model_base import Base
 from src.utils.config import database_connection_logger
 
-__connection: Optional[Callable[[], Session]] = None
-
 
 def global_initializer() -> None:
-    global __connection
+    # database info
+    USER = os.environ.get("SQL_USER")
+    PASSWORD = os.environ.get("SQL_PASSWORD")
+    NAME = os.environ.get("SQL_DATABASE")
+    PORT = os.environ.get("SQL_PORT")
+    HOST = os.environ.get("SQL_HOST")
 
-    if __connection:
-        return
-
-    connection_string = str(
-        os.environ.get(
-            "DATABASE_URL", "sqlite:///backend/db/local_database.db"
-        )
-    )
+    connection_url = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"
 
     database_connection_logger()
 
-    engine = sqlalchemy.create_engine(connection_string, echo=False)
-    __connection = sessionmaker(bind=engine)
+    engine = create_engine(connection_url, echo=True)
 
-    from src.models.user import User
+    if not engine:
+        raise Exception("Engine initialization failed.")
 
     Base.metadata.create_all(engine)
 
-
-def create_session() -> Session:
-    global __connection
-
-    if not __connection:
-        raise Exception(
-            "You must call the global_initializer method before using"
-            + " create_session method."
-        )
-
-    session: Session = __connection()
-    session.expire_on_commit = False
-
-    return session
-
-
-def get_database() -> Session:
-    database = create_session()
-
     try:
-        yield database
+        yield engine
+
     finally:
-        database.close()
+        engine.close()
