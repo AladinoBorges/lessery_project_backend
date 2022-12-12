2e51adda29bf59b58ac3aad57cd974dcd15f0791
Aladino Borges <aladino@huntly.com.br>
2022-12-12 20:32:32 -0300

    feat[controllers]: implements basic user controller
    - implemented Create and Read functions;
    - needs update and delete for full CRUD functions.

---
+++ src/controllers/users.py
@@ -0,0 +1,51 @@
+from sqlalchemy import BigInteger
+from sqlalchemy.orm import Session
+
+from src.models.user import UserModel
+from src.schemas.UserBase import UserCreateSchema, UserReadSchema
+
+
+# ! Create
+def create_user(database: Session, user: UserCreateSchema) -> UserReadSchema:
+    hashed_password: str = user.password
+    new_user: UserReadSchema = UserModel(
+        email=user.email, hashed_password=hashed_password
+    )
+
+    database.add(new_user)
+    database.commit()
+    database.refresh(new_user)
+
+    return new_user
+
+
+# ! Read
+def get_user(database: Session, user_id: BigInteger) -> UserReadSchema:
+    query_result: UserReadSchema = (
+        database.query(UserModel).filter(UserModel.id == user_id).first()
+    )
+
+    return query_result
+
+
+def get_user_by_email(database: Session, user_email: str) -> UserReadSchema:
+    query_result: UserReadSchema = (
+        database.query(UserModel).filter(UserModel.email == user_email).first()
+    )
+
+    return query_result
+
+
+def get_users(
+    database: Session, skip: int = 0, limit: int = 30
+) -> list[UserReadSchema]:
+    query_result: list[UserReadSchema] = (
+        database.query(UserModel).offset(skip).limit(limit).all()
+    )
+
+    return query_result
+
+
+# ! Update
+
+# ! Delete
