45510d75cdb2c81310c124e0c67ee4ff4d5ec2fd
Aladino Borges <aladino@huntly.com.br>
2022-12-12 19:44:16 -0300

    feat[models]: creates basic product model for SQLAlchemy

---
+++ src/models/product.py
@@ -0,0 +1,18 @@
+from sqlalchemy import ARRAY, BigInteger, Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+from src.connections.engine import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True, index=True)
+
+    code = Column(String(200), nullable=False, index=True)
+    name = Column(String(400), nullable=False, index=True)
+    description = Column(String(2000), nullable=False, index=True)
+    owner_id = Column(BigInteger, ForeignKey("users.id"))
+    prices = Column(ARRAY(BigInteger), nullable=True, index=True)
+
+    owner = relationship("User", back_populates="products")
