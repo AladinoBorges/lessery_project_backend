19880947104fd26f4208b14e4d90e63176508f64
Aladino Borges <aladino@huntly.com.br>
2022-12-17 18:27:04 -0300

    feat[controllers]: creates products controller - MSC

--- src/controllers/products.py
+++ src/controllers/products.py
@@ -1,88 +1,53 @@
 from sqlalchemy import BigInteger
-from sqlalchemy.orm import Session
 
-from src.models.product import ProductModel
-from src.schemas.ProductBase import (
-    ProductBaseSchema,
-    ProductCreateSchema,
-    ProductReadSchema,
-)
-
-"""
-    write an util that receives as parameter a product url (ex: amazon) and
-    parses the unique id (removes all information from the url and leaves only
-    the unique id).
-
-    then, write another util that connects with amazon API and retrieves all
-    necessary information for product creation on LESSERY database.
-"""
-
-# TODO: UTILS START
+from src.schemas.ProductBase import ProductReadSchema
+from src.services.products import ProductsService
 
 
-def get_product_unique_id(url: str) -> str | None:
-    return None
+class ProductsController:
+    def create(
+        external_url: str, api_name: str, user_id: BigInteger
+    ) -> ProductReadSchema:
+        new_product: ProductReadSchema = ProductsService.create(
+            external_url, api_name, user_id
+        )
 
+        return new_product
 
-def get_product_info_from_external_website(
-    product_id: str,
-) -> ProductBaseSchema | None:
-    return None
+    def find_all(skip: int, limit: int) -> list[ProductReadSchema]:
+        products: list[ProductReadSchema] = ProductsService.find_all(
+            skip, limit
+        )
 
+        return products
 
-# TODO: UTILS END
+    def find_by_id(product_id: BigInteger) -> ProductReadSchema:
+        product: ProductReadSchema = ProductsService.find_by_id(product_id)
 
+        return product
 
-# ! Create
-def create_product(
-    database: Session, product: ProductCreateSchema, user_id: BigInteger
-) -> ProductReadSchema:
-    new_product: ProductReadSchema = ProductModel(
-        **product.dict(), owner_id=user_id
-    )
+    def find_by_code(product_code: str) -> ProductReadSchema:
+        product: ProductReadSchema = ProductsService.find_by_code(product_code)
 
-    database.add(new_product)
-    database.commit()
-    database.refresh(new_product)
+        return product
 
-    return new_product
+    def find_by_name(product_name: str) -> list[ProductReadSchema]:
+        products: list[ProductReadSchema] = ProductsService.find_by_name(
+            product_name
+        )
 
+        return products
 
-# ! Read
-def get_product(
-    database: Session, product_id: BigInteger
-) -> ProductReadSchema:
-    query_result: ProductReadSchema = (
-        database.query(ProductModel)
-        .filter(ProductModel.id == product_id)
-        .first()
-    )
+    def find_by_category(product_category: str) -> list[ProductReadSchema]:
+        products: list[ProductReadSchema] = ProductsService.find_by_category(
+            product_category
+        )
 
-    return query_result
+        return products
 
+    def find_by_owner_id(owner_id: BigInteger) -> list[ProductReadSchema]:
+        products: list[ProductReadSchema] = ProductsService.find_by_owner_id(
+            owner_id
+        )
 
-def get_product_by_code(
-    database: Session, product_code: str
-) -> ProductReadSchema:
-    query_result: ProductReadSchema = (
-        database.query(ProductModel)
-        .filter(ProductModel.code == product_code)
-        .first()
-    )
-
-    return query_result
-
-
-def get_products(
-    database: Session, skip: int = 0, limit: int = 30
-) -> list[ProductReadSchema]:
-    query_result: list[ProductReadSchema] = (
-        database.query(ProductModel).offset(skip).limit(limit).all()
-    )
-
-    return query_result
-
-
-# ! Update
-
-# ! Delete
+        return products
