04a0f21db84e22e7213c0c7d1076ec16ff498ac5
Aladino Borges <aladino@huntly.com.br>
2022-12-17 18:35:51 -0300

    feat[services]: creates encoder util for services - MSC

---
+++ src/services/utils/encoders.py
@@ -0,0 +1,15 @@
+"""
+    write an util that receives as parameter a unencrypted password and
+    returns an encrypted (hashed) string.
+"""
+
+# TODO: UTILS START
+
+
+def generate_hashed_password(
+    password: str, encryption_algorithm: str = "SHA256"
+) -> str | None:
+    return None
+
+
+# TODO: UTILS END
