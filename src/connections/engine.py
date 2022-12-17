e6f8f0bbf225240395c634874c157f53a44168df
Aladino Borges <aladino@huntly.com.br>
2022-12-17 18:25:30 -0300

    feat[connections]: updates SQLAlchemy engige

--- src/connections/engine.py
+++ src/connections/engine.py
@@ -1,20 +1,21 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
-from src.utils.config import database_connection_logger
-
 # database info
+# ! instalar o dotenv para lidar com as envs:
+"""
+    https://pypi.org/project/python-dotenv
+
+    https://stackoverflow.com/questions/41546883/what-is-the-use-of-python-dotenv
+"""
 USER: str = os.environ.get("SQL_USER")
 PASSWORD: str = os.environ.get("SQL_PASSWORD")
 NAME: str = os.environ.get("SQL_DATABASE")
 PORT: str = os.environ.get("SQL_PORT")
 HOST: str = os.environ.get("SQL_HOST")
 DATABASE_URL: str = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"
-
-database_connection_logger()
 
 engine = create_engine(DATABASE_URL, echo=False)
 
@@ -24,5 +25,3 @@ if not engine:
 LocalSession: Session = sessionmaker(
     autocommit=False, autoflush=True, bind=engine
 )
-
-Base = declarative_base()
