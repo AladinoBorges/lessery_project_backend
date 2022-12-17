83c73d1cba912e7820835f39c529ffb853d2d362
Aladino Borges <aladino@huntly.com.br>
2022-12-17 18:29:43 -0300

    feat[models]: creates users model for MSC architecture

--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,9 @@
 from sqlalchemy import ARRAY, BigInteger, Boolean, Column, Date, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Session, relationship
 
-from src.connections.engine import Base
+from src.models.base import Base
+from src.models.utils.connection import database_connection
+from src.schemas.UserBase import UserCreateSchema, UserReadSchema
 
 
 class UserModel(Base):
@@ -19,3 +21,58 @@ class UserModel(Base):
     logout_log = Column(ARRAY(Date), nullable=True, index=True)
 
     products = relationship("Products", back_populates="owner")
+
+
+class UsersModel:
+    def create(user_data) -> UserCreateSchema:
+        database: Session = database_connection()
+
+        new_user: UserReadSchema = UserModel(
+            name=user_data.name,
+            last_name=user_data.last_name,
+            email=user_data.email,
+            password=user_data.password,
+        )
+
+        database.add(new_user)
+        database.commit()
+        database.refresh(new_user)
+
+        return new_user
+
+    def find_all(skip: int, limit: int) -> list[UserReadSchema]:
+        database: Session = database_connection()
+
+        users: list[UserReadSchema] = (
+            database.query(UserModel).offset(skip).limit(limit).all()
+        )
+
+        return users
+
+    def find_by_id(user_id: BigInteger) -> UserCreateSchema:
+        database: Session = database_connection()
+
+        user: UserReadSchema = (
+            database.query(UserModel).filter(UserModel.id == user_id).first()
+        )
+
+        return user
+
+    def find_by_email(user_email: str) -> UserCreateSchema:
+        database: Session = database_connection()
+
+        user: UserReadSchema = (
+            database.query(UserModel)
+            .filter(UserModel.email == user_email)
+            .first()
+        )
+
+        return user
+
+    """
+    def update(user_id: int, data: UserUpdateSchema) -> UserCreateSchema:
+        database: Session = database_connection()
+
+    def delete(user_id: int, is_active: bool):
+        database: Session = database_connection()
+    """
