99f4ff9a07df44b4da9bc95f499cee8753a22e63
Aladino Borges <aladino@huntly.com.br>
2022-12-17 22:55:32 -0300

    delete[models]: unnecessary model util

--- src/models/utils/connection.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Depends
-from sqlalchemy.orm import Session
-
-from src.utils.connectors.database import connect_to_database
-
-
-def database_connection() -> Session:
-    return Depends(connect_to_database)
