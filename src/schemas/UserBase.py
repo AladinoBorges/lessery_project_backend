602a95cc2e13ddc96850e4268ffb4efc377fc3da
Aladino Borges <aladino@huntly.com.br>
2022-12-18 15:38:25 -0300

    refactor[schemas]: user schema for new architecture

--- src/schemas/UserBase.py
+++ src/schemas/UserBase.py
@@ -1,8 +1,4 @@
-from datetime import date
-
 from pydantic import BaseModel as BaseSchema
-
-from src.schemas.ProductBase import Product
 
 
 class UserBaseSchema(BaseSchema):
@@ -15,25 +11,13 @@ class UserCreateSchema(UserBaseSchema):
     password: str
 
 
-class User(UserBaseSchema):
+class UserReadSchema(UserBaseSchema):
     id: int
     is_active: bool
-    products: list[Product] = []
-
-    login_log: list[date] = []
-    logout_log: list[date] = []
 
     class Config:
         orm_mode: bool = True
 
 
-class UserReadSchema(BaseSchema):
-    success: bool
-    status_code: int
-    data: User
-
-
 class UsersReadSchema(BaseSchema):
-    success: bool
-    status_code: int
-    data: list[User]
+    users: list[UserReadSchema]
