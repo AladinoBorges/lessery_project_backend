7995162efca5e2bd4f0936a364b64ecac6168083
Aladino Borges <aladino@huntly.com.br>
2022-12-18 14:13:32 -0300

    feat[utilities]: updates logs handlers (now uses OOP)

--- src/utilities/logs/handlers.py
+++ src/utilities/logs/handlers.py
@@ -3,25 +3,23 @@ from functools import lru_cache
 
 from pydantic import BaseSettings
 
-from src.classes.SettingsClass import SettingsClass
+from src.utilities.informative.Application import ApplicationInformation
 
 log: logging.Logger = logging.getLogger("uvicorn")
 
 
-def startup_event() -> None:
-    log.info("[LESSERY] - Starting up the application ...")
+class LogsHandlers:
+    def startup() -> None:
+        log.info("[LESSERY] - Starting up the application ...")
 
+    def shutdown() -> None:
+        log.info("[LESSERY] - Closing the application ...")
 
-def shutdown_event() -> None:
-    log.info("[LESSERY] - Closing the application ...")
+    def database_connection() -> None:
+        log.info("[LESSERY] - Connecting to database ...")
 
+    @lru_cache
+    def get_settings() -> BaseSettings:
+        log.info("[LESSERY] - Loading config settings from environment ...")
 
-def database_connection_logger() -> None:
-    log.info("[LESSERY] - Connecting to database ...")
-
-
-@lru_cache
-def get_settings() -> BaseSettings:
-    log.info("[LESSERY] - Loading config settings from environment ...")
-
-    return SettingsClass()
+        return ApplicationInformation()
