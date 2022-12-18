b0b8210182b27b4797abb88f721b014775d7c8d4
Aladino Borges <aladino@huntly.com.br>
2022-12-18 14:15:09 -0300

    feat[main]: updates main file for using correct api endpoints (status router) and log handlers

--- src/main.py
+++ src/main.py
@@ -1,30 +1,35 @@
 from fastapi import FastAPI
 
-from src.api.status import status_router
 from src.api.users import users_router
 from src.connections.engine import engine
 from src.models.base import Base
-from src.utilities.logs.handlers import shutdown_event, startup_event
+from src.routers.ApiStatus import ApiStatus
+from src.utilities.logs.handlers import LogsHandlers
 
 
 def create_application() -> FastAPI:
     Base.metadata.create_all(bind=engine)
 
     application = FastAPI()
+
     application.include_router(users_router)
-    application.include_router(status_router)
+    application.include_router(ApiStatus.router)
 
     return application
 
 
 app: FastAPI = create_application()
 
+""" @app.exception_handler(AppExceptionCase)
+def handle_exceptions(request, error):
+    return app_exception_handler(request, error) """
+
 
 @app.on_event("startup")
 def generate_startup_log() -> None:
-    startup_event()
+    LogsHandlers.startup()
 
 
 @app.on_event("shutdown")
 def generate_shutdown_log() -> None:
-    shutdown_event()
+    LogsHandlers.shutdown()
