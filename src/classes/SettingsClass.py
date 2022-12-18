ecf3ed7980f48ebd52345b5e2d2d56d27f0972f5
Aladino Borges <aladino@huntly.com.br>
2022-12-18 14:00:35 -0300

    feat[classes]: deletes settings

--- src/classes/SettingsClass.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from pydantic import BaseSettings
-
-
-class SettingsClass(BaseSettings):
-    version: str = os.getenv("APP_VERSION", "1.0.0")
-    testing: bool = os.getenv("TESTING", False)
-    environment: str = os.getenv("ENVIRONMENT", "dev")
