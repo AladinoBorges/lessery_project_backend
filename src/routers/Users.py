e9919b3a612e47fff03603ee572ce8caa554788e
Aladino Borges <aladino@huntly.com.br>
2022-12-18 15:38:13 -0300

    refactor[routers]: user router for new architecture

--- src/routers/Users.py
+++ src/routers/Users.py
@@ -2,41 +2,57 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from src.connections.Database import Database
-from src.middlewares.users import UsersMiddleware
+from src.models.users import UsersModel
 from src.schemas.UserBase import (
     UserCreateSchema,
     UserReadSchema,
     UsersReadSchema,
 )
+from src.services.users import UsersService
+from src.utilities.responses.handlers import Default
 
 
 class UsersRouter:
     connection: Session = Database.connect
     router = APIRouter(tags=["users"], prefix="/v1/users")
+    response_type = (
+        UsersReadSchema | UserReadSchema | dict[str, bool | int | str]
+    )
 
-    @router.post("/", response_model=UserReadSchema | dict[str, int | str])
+    @router.post("/", response_model=response_type)
     def create(
         user_data: UserCreateSchema, database: Session = Depends(connection)
     ) -> UserReadSchema | HTTPException:
-        new_user = UsersMiddleware.create(user_data, database)
+        data = UsersService.create(user_data)
 
-        return new_user
+        new_user = UsersModel.create(data, database)
 
-    @router.get("/", response_model=UsersReadSchema | dict[str, int | str])
+        return Default.unique(new_user, "error creating a new user.", 404)
+
+    @router.get("/", response_model=response_type)
     def find_all(
         skip: int = 0, limit: int = 13, database: Session = Depends(connection)
     ) -> UserReadSchema | HTTPException:
-        users = UsersMiddleware.find_all(skip, limit, database)
+        users = UsersModel.find_all(skip, limit, database)
 
-        return users
+        return Default.multiple(users, "users not found.", 404)
 
-    @router.get(
-        "/{user_id}", response_model=UserReadSchema | dict[str, int | str]
-    )
+    @router.get("/{user_id}", response_model=response_type)
     def find_by_id(
         user_id: int, database: Session = Depends(connection)
     ) -> UserReadSchema | HTTPException:
+        data = UsersService.find_by_id(user_id)
 
-        user = UsersMiddleware.find_by_id(user_id, database)
+        user = UsersModel.find_by_id(data, database)
 
-        return user
+        return Default.unique(user, "user not found.", 404)
+
+    @router.get("/?{user_mail}", response_model=response_type)
+    def find_by_email(
+        user_mail: int, database: Session = Depends(connection)
+    ) -> UserReadSchema | HTTPException:
+        data = UsersService.find_by_email(user_mail)
+
+        user = UsersModel.find_by_email(data, database)
+
+        return Default.unique(user, "user not found.", 404)
