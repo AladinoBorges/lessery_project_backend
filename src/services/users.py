72308c32c93484b96996744239c8be4c5009d092
Aladino Borges <aladino@huntly.com.br>
2022-12-17 22:52:44 -0300

    feat[services]: implements database session on users service

--- src/services/users.py
+++ src/services/users.py
@@ -1,6 +1,6 @@
-from sqlalchemy import BigInteger
+from sqlalchemy.orm import Session
 
-from src.models.user import UsersModel
+from src.models.users import UsersModel
 from src.schemas.UserBase import UserCreateSchema, UserReadSchema
 from src.services.utils.default_response import response
 from src.services.utils.encoders import generate_hashed_password
@@ -15,13 +15,15 @@ class UsersService:
 
         return response(data, "error creating a new user.", 404, 200)
 
-    def find_all(skip: int, limit: int):
-        data = UsersModel.find_all(skip, limit)
+    def find_all(
+        skip: int, limit: int, database: Session
+    ) -> list[UserReadSchema]:
+        data = UsersModel.find_all(skip, limit, database)
 
         return response(data, "users not found.", 404, 200)
 
-    def find_by_id(user_id: BigInteger) -> UserReadSchema:
-        data = UsersModel.find_by_id(user_id)
+    def find_by_id(user_id: int, database: Session) -> UserReadSchema:
+        data = UsersModel.find_by_id(user_id, database)
 
         return response(data, "user not found.", 404, 200)
 
@@ -32,7 +34,7 @@ class UsersService:
 
     """
     def update(
-      user_id: BigInteger, data: UpdateUserSchema
+      user_id: int, data: UpdateUserSchema
     ) -> UserReadSchema:
         data = UsersModel.update(user_id, data)
 
@@ -40,7 +42,7 @@ class UsersService:
 
 
     def delete(
-      user_id: BigInteger, is_active: bool
+      user_id: int, is_active: bool
     ) -> UserDeleteSchema:
         user = UsersModel.delete(user_id, active_status)
 
