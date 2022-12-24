fbaa9b4576dc9ff427ef93eb6f312f5bb8963354
Aladino Borges <aladino@huntly.com.br>
2022-12-24 20:10:29 -0300

    feat[services]: updates user service and now uses data validation utility and correct password encryption

--- src/services/users.py
+++ src/services/users.py
@@ -1,46 +1,33 @@
 from fastapi import HTTPException
 
-from src.helpers.emails import email_validation
-from src.schemas.UserBase import UserBaseSchema, UserReadSchema
-from src.services.utils.encoders import generate_hashed_password
+from src.schemas.UserBase import UserCreateSchema
+from src.services.utils.users.validations import Validate
+from src.utilities.encryption.encoders import generate_hashed_password
 from src.utilities.errors.handlers import Exceptions
 
 
 class UsersService:
-    def create(user: UserBaseSchema, password: str) -> str | HTTPException:
-        if not email_validation(user.email):
-            return Exceptions.http("please, insert a valid email.", 400)
+    def create(user: UserCreateSchema) -> bytes | HTTPException:
+        try:
+            Validate.name(user.name)
+            Validate.email(user.email)
+            Validate.password(user.password)
+            Validate.lastname(user.last_name)
 
-        if not user.name or isinstance(user.name, int) or len(user.name) == 0:
-            return Exceptions.http("please, insert a valid name.", 400)
+            hashed_password = generate_hashed_password(user.password)
 
-        if (
-            not user.last_name
-            or isinstance(user.last_name, int)
-            or len(user.last_name) == 0
-        ):
-            return Exceptions.http("please, insert a valid last name.", 400)
+            if not hashed_password:
+                return Exceptions.http(
+                    "Erro ao encriptar a sua password.", 400
+                )
 
-        if not password or len(password) < 8:
-            message = (
-                "please, insert a valid password with"
-                + " a minimum of 8 characters."
-            )
+            return hashed_password
 
-            return Exceptions.http(message, 400)
+        except (HTTPException) as Error:
+            return Error
 
-        hashed_password = generate_hashed_password(password)
+    def find_by_id(user_id: int) -> int | HTTPException:
+        return Validate.identifier(user_id)
 
-        return hashed_password
-
-    def find_by_id(user_id: int) -> UserReadSchema | HTTPException:
-        if not isinstance(user_id, int):
-            return Exceptions.http("user id needs to be an integer.", 400)
-
-        return user_id
-
-    def find_by_email(email: str) -> UserReadSchema | HTTPException:
-        if not email_validation(email):
-            return Exceptions.http("please, insert a valid email.", 400)
-
-        return email
+    def find_by_email(email: str) -> str | HTTPException:
+        return Validate.email(email)
