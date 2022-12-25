844f794c5f44531e7bc8f7cdb42e73c5eab5b12b
Aladino Borges <aladino@huntly.com.br>
2022-12-24 21:53:10 -0300

    feat[models]: creates user update method

--- src/models/users.py
+++ src/models/users.py
@@ -5,6 +5,7 @@ from src.schemas.UserBase import (
     UserCreateHashedSchema,
     UserCreateSchema,
     UserReadSchema,
+    UserUpdateSchema,
 )
 
 
@@ -20,6 +21,7 @@ class UsersModel:
         )
 
         database.add(new_user)
+        database.flush()
         database.commit()
         database.refresh(new_user)
 
@@ -47,5 +49,25 @@ class UsersModel:
             .filter(UserModel.email == user_email)
             .first()
         )
+
+        return user
+
+    def update(
+        user_id: int, data: UserUpdateSchema, database: Session
+    ) -> UserReadSchema:
+        user: UserReadSchema = (
+            database.query(UserModel).filter(UserModel.id == user_id).first()
+        )
+
+        if not user:
+            return None
+        else:
+            update_data = data.dict(exclude_unset=True)
+
+            for key, value in update_data.items():
+                setattr(user, key, value)
+
+            database.commit()
+            database.refresh(user)
 
         return user
